a731c10bc23c5dd8cfa39e7ad1176c6465f2aaf6
cholker ajay kumar <cholkerajaykumar@gmail.com>
2021-12-04 16:41:03 +0530

    add business form

--- src/pages/AddBusiness/index.js
+++ src/pages/AddBusiness/index.js
@@ -1,10 +1,60 @@
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
 function AddBusiness() {
-    return (
-      <div>
-        Business
+  return (
+    <>
+      <h3 className="text-center">Add Business</h3>
+      <div className="d-flex justify-content-center">
+        <form className="d-flex mt-3 flex-column">
+          <div className="d-flex justify-content-around mb-4 gap-5">
+            <TextField
+              id="outlined-basic"
+              label="Business Name"
+              variant="outlined"
+            />
+            <TextField
+              id="outlined-basic"
+              label="Business Type"
+              variant="outlined"
+            />
+          </div>
+          <div className="d-flex justify-content-around mb-4 gap-5">
+            <TextField
+              id="outlined-basic"
+              label="Mobile Number"
+              variant="outlined"
+              type="number"
+            />
+            <TextField id="outlined-basic" label="Email" variant="outlined" />
+          </div>
+          <div className="d-flex justify-content-around mb-4">
+            <TextField
+              fullWidth
+              id="outlined-basic"
+              label="Address"
+              variant="outlined"
+              multiline
+              rows={2}
+            />
+          </div>
+          <div className="border p-4 mb-2">
+            <h6 className="text-center mb-3">Services</h6>
+            <TextField
+              id="outlined-basic"
+              label="Add Your Service"
+              variant="outlined"
+              size="small"
+            />
+          </div>
+          <div className="text-center">
+            <Button variant="outlined">Submit</Button>
+          </div>
+        </form>
       </div>
-    );
-  }
-  
-  export default AddBusiness;
-  
+    </>
+  );
+}
+
+export default AddBusiness;
