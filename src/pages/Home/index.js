b561512ada70b1f816efea4269d18d9af2ea197e
dasavarambharadwaj <dasavarambharadwaj@gmail.com>
2021-12-04 20:19:31 +0530

    Search Implementation and connecting to db

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,18 @@
-import Typography from "@mui/material/Typography";
-import {
-  Input,
-  InputAdornment,
-  OutlinedInput,
-  Paper,
-  Popover,
-} from "@mui/material";
+import { CircularProgress, InputAdornment, OutlinedInput, Paper, Popover } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import "./Home.scss";
 import { useState } from "react";
+import ApiServices from "../../services/apiServices";
 function Home() {
   const [open, setOpen] = useState(false);
   const [anchorPos, setAnchorPos] = useState(null);
   const [popupWidth, setpopupWidth] = useState(0);
-
-  const handleShow = (event) => {
+  const [searchResults, setSearchResults] = useState([]);
+  const [noData, setNoData] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [debounceTimeoutHandle, setDebounceTimeoutHandle] = useState(null);
+  let apiServices = new ApiServices();
+  const handleShow = () => {
     let element = document.querySelectorAll(".search-field")[0];
     setAnchorPos({
       top: element.offsetTop + element.offsetHeight + 5,
@@ -24,8 +22,32 @@ function Home() {
     setOpen(true);
   };
   const handleClose = () => {
-    setAnchorPos(null);
     setOpen(false);
+  };
+  const getDataOnSearch = async (event) => {
+    if (debounceTimeoutHandle) {
+      clearTimeout(debounceTimeoutHandle);
+      setDebounceTimeoutHandle(null);
+    }
+    if (event.target.value !== "") {
+      setLoading(true);
+      setDebounceTimeoutHandle(
+        setTimeout(async () => {
+          let result = await apiServices.get(
+            "http://localhost:3001/customers",
+            { search: event.target.value }
+          );
+          setLoading(false);
+          setNoData(result.data.length === 0);
+          setSearchResults(result.data);
+          setOpen(true);
+        }, 1000)
+      );
+    } else {
+      setNoData(false);
+      setLoading(false);
+      setSearchResults([]);
+    }
   };
   return (
     <div className="text-center">
@@ -36,7 +58,10 @@ function Home() {
         <OutlinedInput
           className="mt-3 search-field"
           placeholder="Search"
-          onFocus={handleShow}
+          onClick={handleShow}
+          onChange={(event) => {
+            getDataOnSearch(event);
+          }}
           startAdornment={
             <InputAdornment position="start">
               <SearchIcon></SearchIcon>
@@ -53,11 +78,34 @@ function Home() {
           anchorPosition={anchorPos}
           disableAutoFocus
         >
-          <Paper style={{ width: popupWidth }} className="p-2">
-            Content of the popover
-          </Paper>
-          <Paper style={{ width: popupWidth }} className="p-2">
-            Content of the popover
+          <Paper
+            style={{ width: popupWidth }}
+            className="popover-results-container p-2"
+          >
+            {noData ? (
+              <div class="d-flex">
+                <span className="mx-auto">No Data</span>
+              </div>
+            ) : loading ? (
+              <div class="d-flex">
+                <CircularProgress className="mx-auto" />
+              </div>
+            ) : searchResults.length === 0 ? (
+              <div class="d-flex">
+                <span className="mx-auto">Search for business</span>
+              </div>
+            ) : (
+              searchResults.map((item) => (
+                <div key={item.id} className="w-100 d-flex p-2">
+                  <span className="my-auto">{item.name}</span>
+                  {item.category && (
+                    <span className={"category-box py-2 px-3 ms-2 category-box-" + item.category.toLowerCase()}>
+                      {item.category}
+                    </span>
+                  )}
+                </div>
+              ))
+            )}
           </Paper>
         </Popover>
       </div>
